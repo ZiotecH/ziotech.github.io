d048209fc2b0d9bb4bcb7859c65deb10fdab4744
BuildTools <unconfigured@null.spigotmc.org>
2021-10-04 22:37:10 +0200

    changed to nextPurchase()

--- zCookies/main.js
+++ zCookies/main.js
@@ -317,7 +317,7 @@ Game.registerMod("zCookies", {
     zInfoAreaLastPlant.onclick = zCookies.zFunctions.showLastPlant;
     zInfoAreaWrinklerInfo.onclick = zCookies.zFunctions.wrinklerHelp;
     zInfoAreaProgressPercent.onclick = zCookies.zFunctions.toggleSlowMode;
-    zCookies.zFunctions.nextProduct(true);
+    zCookies.zFunctions.nextPurchase(false);
     Game.Notify(`zCookies v:` + zCookies.variables.zCookiesVersion + ` loaded.`, `Enjoy your automation.`, [16, 5], true);
     zCookies.mainLoop()
   },
