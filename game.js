9776b02586ceb0a6ab0d2ad38437b6f42baadc60
Danboy Bebace <ziotechakaxixxorz@gmail.com>
2021-10-25 17:45:42 +0200

    Added randomized positions for completed starfalls.

--- game.js
+++ game.js
@@ -10,13 +10,13 @@ kitten.addEventListener("click",eatBread);
 function eatBread(){
     noms++
     if(noms%100==0 && !(starList.length >= 200) ){
-        starGen(...rngHandler());
+        starList.push(starGen(...rngHandler()));
     }
     if(noms%500==0 && !(heartList5h.length >= 200)){
-        heartGen5h(...rngHandler());
+        HeartList5h.push(heartGen5h(...rngHandler()));
     }
     if(noms%1e3==0 && !(heartList1k.length >= 200)){
-        heartGen1k(...rngHandler());
+        heartList1k.push(heartGen1k(...rngHandler()));
     }
     if(noms >= 1e4){
         displayElm.innerText = "Chomps: "+noms.toExponential(3);
@@ -27,6 +27,7 @@ function eatBread(){
 }
 
 function starGen(x,s,d,r,w){
+    //console.log("Creating star with "+[x,s,d,r,w]+".")
     /*
     x pos
     s speed
@@ -46,12 +47,13 @@ function starGen(x,s,d,r,w){
     tmp.style.transform = "rotate("+r+"deg)";
     tmp.style.animationDelay = w+"s";
     tmp.classList.add("star");
+    tmp.addEventListener("animationend",function(obj){objectRestructure(obj);});
     document.getElementsByTagName("body")[0].appendChild(tmp);
-    starList.push(tmp);
-    return
+    return tmp
 }
 
 function heartGen1k(x,s,d,r,w){
+    //console.log("Creating heart_1k with "+[x,s,d,r,w]+".")
     /*
     x pos
     s speed
@@ -71,12 +73,13 @@ function heartGen1k(x,s,d,r,w){
     tmp.style.transform = "rotate("+r+"deg)";
     tmp.style.animationDelay = w+"s";
     tmp.classList.add("heart_1k");
+    tmp.addEventListener("animationend",function(obj){objectRestructure(obj);});
     document.getElementsByTagName("body")[0].appendChild(tmp);
-    heartList1k.push(tmp);
-    return
+    return tmp
 }
 
 function heartGen5h(x,s,d,r,w){
+    //console.log("Creating heart_5h with "+[x,s,d,r,w]+".")
     /*
     x pos
     s speed
@@ -96,9 +99,9 @@ function heartGen5h(x,s,d,r,w){
     tmp.style.transform = "rotate("+r+"deg)";
     tmp.style.animationDelay = w+"s";
     tmp.classList.add("heart_5h");
+    tmp.addEventListener("animationend",function(obj){objectRestructure(obj);});
     document.getElementsByTagName("body")[0].appendChild(tmp);
-    heartList5h.push(tmp);
-    return
+    return tmp
 }
 
 
@@ -113,4 +116,23 @@ function rngHandler(){
         if(S < 5){S = 5}
         if(D < 25){D = 25}
         return [X,S,D,R,W]
+}
+
+function objectRestructure(caller){
+    var classList = caller.target.classList;
+    var index;
+    caller.target.remove()
+    if(classList.contains("star")){
+        index = starList.indexOf(caller.target);
+        starList[index] = starGen(...rngHandler());
+    }else if(classList.contains("heart_1k")){
+        index = heartList1k.indexOf(caller.target);
+        heartList1k[index] = heartGen1k(...rngHandler());
+    }else if(classList.contains("heart_5h")){
+        index = heartList5h.indexOf(caller.target);
+        heartList5h[index] = heartGen5h(...rngHandler());
+    }
+    //console.log(caller.target)
+    //console.log(caller)
+    return
 }
